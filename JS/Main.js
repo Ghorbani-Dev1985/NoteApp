d1586a9e6cc02ad671064b325fdba1dc29334b02
Ghorbani-Dev.ir <ghorbani.dev1985@gmail.com>
2023-11-22 11:04:07 +0330

    feat: develop SetLocalStorage func for save notes in localstorage and NoteGenerator for generate new note

--- JS/Main.js
+++ JS/Main.js
@@ -38,7 +38,54 @@ function AddNewNote() {
     RemoveAlert();
   }
 }
+function SetLocalStorage(noteList) {
+  localStorage.setItem("Note", JSON.stringify(noteList));
+}
 
+function NoteGenerator(NoteLists) {
+  if (NoteLists.length > 0) {
+    let NewDivEle, SpanEle, TrashImgEle, TickImgEle, EditImgEle, IconDiv;
+    NoteItemsBody.innerHTML = "";
+    NoteLists.forEach((NoteList) => {
+      NewDivEle = $.createElement("div");
+      IconDiv = $.createElement("div");
+      SpanEle = $.createElement("span");
+      TrashImgEle = $.createElement("img");
+      TickImgEle = $.createElement("img");
+      EditImgEle = $.createElement("img");
+      NewDivEle.className = "NoteItem";
+      IconDiv.className =
+        "w-full gap-5 flex content-end items-center border p-10";
+      TickImgEle.setAttribute("src", "./Assets/Images/tick.svg");
+      EditImgEle.setAttribute("src", "./Assets/Images/edit.svg");
+      TrashImgEle.setAttribute("src", "./Assets/Images/trash.svg");
+      TrashImgEle.addEventListener("click", () =>
+        DeleteNote(NoteList.id, NoteList.NoteTitle)
+      );
+      TickImgEle.addEventListener("click", () => CompleteNote(NoteList.id));
+      EditImgEle.addEventListener("click", () =>
+        EditNote(NoteList.id, NoteList.NoteTitle)
+      );
+      SpanEle.textContent = NoteList.NoteTitle;
+      if (NoteList.isCompleted) {
+        SpanEle.className = "completed";
+      } else {
+        SpanEle.className = "";
+      }
+      IconDiv.append(TickImgEle, EditImgEle, TrashImgEle);
+      NewDivEle.append(SpanEle, IconDiv);
+      NewDivEle.style.backgroundColor = NoteList.NoteBgColor;
+      NoteItemsBody.append(NewDivEle);
+      NoteWrapper.style.backgroundColor = "rgba(40 41 61 / 1)";
+      RemoveAlert();
+    });
+  } else {
+    NewDivEle = $.createElement("div");
+    NewDivEle.innerHTML = "هیچ یادداشتی ثبت نگردیده است.";
+    NewDivEle.className = "NullNoteAlert";
+    NoteItemsBody.append(NewDivEle);
+  }
+}
 
 
 // EventListener
